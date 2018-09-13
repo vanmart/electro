653ebe23620543e5c33677e0acf1b68542e4e5f5
Ivan Martinez C <vanmartc@gmail.com>
2018-09-12 19:50:14 -0500

    using devise gem for user authentication, we generate the home controller with 3 methods and their views for minimal navigation and testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :only => [:landing, :byebye]
+  
+  def after_sign_in_path_for(resource)
+    welcome_path
+  end
+
+  def after_sign_out_path_for(resource)
+    byebye_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  authenticated :user do
+    root to: 'home#welcome'
+  end
+  
+  unauthenticated :user do
+    root to: 'home#landing'
+  end
+
+  get 'landing', to: 'home#landing', as: :landing
+  get 'welcome', to: 'home#welcome', as: :welcome
+  get 'byebye', to: 'home#byebye', as: :byebye
+
 end
